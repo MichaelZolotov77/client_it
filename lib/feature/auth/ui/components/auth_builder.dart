0e95a7e9fc3e169532eb828fd77c809d80136ac8
MichaelZolotov77 <michaelzol@yandex.ru>
2023-07-14 15:56:15 +0300

    5.10

--- lib/feature/auth/ui/components/auth_builder.dart
+++ lib/feature/auth/ui/components/auth_builder.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:client_it/feature/auth/domain/auth_state/auth_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
