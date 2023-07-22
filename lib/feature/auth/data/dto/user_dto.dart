613c39c46b7fd20be6303faf13c347cd390a6c5d
MichaelZolotov77 <michaelzol@yandex.ru>
2023-07-22 21:08:40 +0300

    5.18

--- lib/feature/auth/data/dto/user_dto.dart
+++ lib/feature/auth/data/dto/user_dto.dart
@@ -1,3 +1,4 @@
+import 'package:client_it/feature/auth/domain/entities/user_entity/user_entity.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'user_dto.g.dart';
@@ -22,4 +23,14 @@ class UserDto {
       _$UserDtoFromJson(json);
 
   Map<String, dynamic> toJson() => _$UserDtoToJson(this);
+
+  UserEntity toEntity() {
+    return UserEntity(
+      email: email.toString(),
+      username: username.toString(),
+      id: id.toString(),
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+    );
+  }
 }
